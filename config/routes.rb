46846be7f2e7dd5b739cf4478b11921b16cc47b2
kkoji <ypcjh662@yahoo.co.jp>
2018-02-18 01:32:59 +0900

    掲示板新規作成ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'boards#index'
+  get 'boards', to: 'boards#index'
+  get 'boards/new', to: 'boards#new'
 end
