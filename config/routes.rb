2c5893b2765bc306706c54fb39857ea23e81658d
k-kojima <ypcjh662@yahoo.co.jp>
2018-03-08 22:41:58 +0900

    リソースベースルーティングの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'boards', to: 'boards#index'
-  get 'boards/new', to: 'boards#new'
-  post 'boards', to: 'boards#create'
-  get 'boards/:id', to: 'boards#show'
+  resources :boards, only: [:index, :new, :create, :show]
 end
