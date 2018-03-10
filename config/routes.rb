c28490c6883c7159193934089fcbd3a215569ce9
k-kojima <ypcjh662@yahoo.co.jp>
2018-03-10 23:35:29 +0900

    掲示板削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :boards, only: [:index, :new, :create, :show, :edit, :update]
+  resources :boards
 end
