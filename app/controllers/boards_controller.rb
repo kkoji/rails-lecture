bf20c589f492e0ade702c487dfa36b0402945849
kkoji <ypcjh662@yahoo.co.jp>
2018-03-10 23:35:29 +0900

    掲示板削除機能の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,7 +8,9 @@ class BoardsController < ApplicationController
   end
 
   def create
-    Board.create(board_params)
+    board = Board.create(board_params)
+
+    redirect_to board
   end
 
   def show
@@ -24,6 +26,13 @@ class BoardsController < ApplicationController
     board.update(board_params)
 
     redirect_to board
+  end
+
+  def destroy
+    board = Board.find(params[:id])
+    board.delete
+
+    redirect_to boards_path
   end
 
   private
