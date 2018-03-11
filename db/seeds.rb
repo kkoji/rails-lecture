30d259ae25edac35edcf16f625f60d9ebbfbe6c8
kkoji <ypcjh662@yahoo.co.jp>
2018-03-11 12:18:27 +0900

    ページネーション追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env == 'development'
+  (1..50).each do |i|
+    Board.create(name: "ユーザー#{i}", title: "タイトル#{i}", body: "本文#{i}")
+  end
+end
