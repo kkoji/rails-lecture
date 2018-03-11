3a561e8c01313ad6f7be1328c33c6582d92888cd
k-kojima <ypcjh662@yahoo.co.jp>
2018-03-11 22:20:05 +0900

    フラッシュでメッセージの表示を追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -11,6 +11,7 @@ class BoardsController < ApplicationController
 
   def create
     board = Board.create(board_params)
+    flash[:notice] = "「#{board.title}」の掲示板を作成しました"
     redirect_to board
   end
 
@@ -27,7 +28,7 @@ class BoardsController < ApplicationController
 
   def destroy
     @board.delete
-    redirect_to boards_path
+    redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました" }
   end
 
   private
