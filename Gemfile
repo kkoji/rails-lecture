0d4695b54d80cfbf9009020854ef763d233ee234
k-kojima <ypcjh662@yahoo.co.jp>
2018-03-14 21:24:13 +0900

    Annotate gemを追加

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'pry-byebug'
+  gem 'annotate'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  body       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class BoardTest < ActiveSupport::TestCase
