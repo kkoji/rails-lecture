674872e7bc90cd35e4e2d343d6027df29402890d
kkoji <ypcjh662@yahoo.co.jp>
2018-03-14 21:24:13 +0900

    Annotate gemを追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  body       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Board < ApplicationRecord
   validates :name, presence: true, length: { maximum: 10 }
   validates :title, presence: true, length: { maximum: 30 }
