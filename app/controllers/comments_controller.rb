fc6ca657ded3c4aabb97a26f46f6c77712ec431f
kkoji <ypcjh662@yahoo.co.jp>
2018-03-17 14:56:14 +0900

    コメントの保存機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,23 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash[:notice] = 'コメントを投稿しました'
+      redirect_to comment.board
+    else
+      redirect_to :back, flash: {
+        comment: comment,
+        error_messages: comment.errors.full_messages
+      }
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:board_id, :name, :comment)
   end
 end
