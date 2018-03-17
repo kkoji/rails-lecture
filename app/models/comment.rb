6ea501457d930a242c9eacb433f5dfe974d57603
k-kojima <ypcjh662@yahoo.co.jp>
2018-03-17 21:31:35 +0900

    Commentモデルへのバリデーション追加&エラー表示実装

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -20,4 +20,7 @@
 
 class Comment < ApplicationRecord
   belongs_to :board
+
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :comment, presence: true, length: { maximum: 1000 }
 end
