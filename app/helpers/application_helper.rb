ce55783597c673c22c662b1d007e14c46ed7edb4
kkoji <ypcjh662@yahoo.co.jp>
2018-03-23 16:05:24 +0900

    ヘッダーメニューの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def header_link_item(name, path)
+    class_name = 'nav-item'
+    class_name << ' active' if current_page?(path)
+
+    content_tag :li, class: class_name do
+      link_to name, path, class: 'nav-link'
+    end
+  end
 end
