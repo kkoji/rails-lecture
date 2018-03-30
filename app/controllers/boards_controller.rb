fe487eae737f4e2b16ec516f57420f3820f1d70e
kkoji <ypcjh662@yahoo.co.jp>
2018-03-30 14:02:39 +0900

    タグから掲示板を検索する機能の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,8 @@ class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
-    @boards = Board.page(params[:page])
+    @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
+    @boards = @boards.page(params[:page])
   end
 
   def new
