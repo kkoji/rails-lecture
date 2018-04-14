e42d28424424c6fd0a2f35ff5c159fa206f59c1f
kkoji <ypcjh662@yahoo.co.jp>
2018-04-14 14:06:26 +0900

    dependentオプションの設定追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -43,7 +43,7 @@ class BoardsController < ApplicationController
   end
 
   def destroy
-    @board.delete
+    @board.destroy
     redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました" }
   end
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -9,6 +9,6 @@
 #
 
 class Tag < ApplicationRecord
-  has_many :board_tag_relations
+  has_many :board_tag_relations, dependent: :delete_all
   has_many :boards, through: :board_tag_relations
 end
