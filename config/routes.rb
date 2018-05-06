2f88a34bb4729fd466c69ede5b7ae4195c8647c2
kkoji <ypcjh662@yahoo.co.jp>
2018-05-07 05:25:17 +0900

    ユーザー認証で使用するルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/me'
-
-  get 'home/index'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
