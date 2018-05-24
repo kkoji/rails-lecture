1160c9548977599071a00f91c9fae60d61786112
kkoji <ypcjh662@yahoo.co.jp>
2018-05-24 17:43:11 +0900

    UserModelの認証設定とバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,16 @@
 #
 
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 16 },
+    format: {
+      with: /\A[a-z0-9]+\z/,
+      message: 'は小文字英数字で入力してください'
+    }
+  validates :password,
+    length: { minimum: 8 }
 end
