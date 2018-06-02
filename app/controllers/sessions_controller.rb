0dc4ed2d3be9db937d26e26eda57a8ae5e295146
k-kojima <ypcjh662@yahoo.co.jp>
2018-06-02 11:01:51 +0900

    ログイン、ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      render 'home/index'
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
