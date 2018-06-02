1257578a334796bc206690241b2b6459f8ace221
k-kojima <ypcjh662@yahoo.co.jp>
2018-06-02 11:51:14 +0900

    ログインユーザーの取得とマイページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+
+  private
+
+  def current_user
+    return unless session[:user_id]
+    @current_user = User.find_by(id: session[:user_id])
+  end
 end
