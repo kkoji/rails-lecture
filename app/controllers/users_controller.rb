7a91470b4ca706a4c4f33ac29cf715a08a4f8f2c
k-kojima <ypcjh662@yahoo.co.jp>
2018-06-02 09:57:19 +0900

    ユーザー登録機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    @user = User.new(flash[:user])
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      redirect_to :back, flash: {
+        user: user,
+        error_messages: user.errors.full_messages
+      }
+    end
   end
 
   def me
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
