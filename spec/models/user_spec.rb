327d002ee66347018aafc64c5d52668e83b04bed
Kazuya Kojima <kkojima@improver.me>
2018-11-28 15:14:57 +0900

    テストコード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,11 +18,31 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe '#age' do
+    before do
+      allow(Time.zone).to receive(:now).and_return(Time.zone.parse('2018/04/01'))
+    end
+
     context '20年前の生年月日の場合' do
       let(:user) { User.new(birthday: Time.zone.now - 20.years) }
 
       it '年齢が20歳であること' do
         expect(user.age).to eq 20
+      end
+    end
+
+    context '10年前に生まれた場合でちょうど誕生日の場合' do
+      let(:user) { User.new(birthday: Time.zone.parse('2008/04/01')) }
+
+      it '年齢が10歳であること' do
+        expect(user.age).to eq 10
+      end
+    end
+
+    context '10年前に生まれた場合で誕生日が来ていない場合' do
+      let(:user) { User.new(birthday: Time.zone.parse('2008/04/02')) }
+
+      it '年齢が9歳であること' do
+        expect(user.age).to eq 9
       end
     end
   end
