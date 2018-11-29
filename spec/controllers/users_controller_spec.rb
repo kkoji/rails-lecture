6552d40b870fcc8ba627dfef888fa4281e556969
Kazuya Kojima <kkojima@improver.me>
2018-11-29 16:34:55 +0900

    UsersControllerのcreateアクションをテスト

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe 'GET #new' do
+    before { get :new }
 
+    it 'レスポンスコードが200であること' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'newテンプレートをレンダリングすること' do
+      expect(response).to render_template :new
+    end
+
+    it '新しいuserオブジェクトがビューに渡されること' do
+      expect(assigns(:user)).to be_a_new User
+    end
+  end
 end
